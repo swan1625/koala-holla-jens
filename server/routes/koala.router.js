9662e9c6a9ecf1057ebac5e382eaac56ce5b1aab
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2022-02-10 14:55:05 -0600

    Added a router GET to get data from koalas database

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const koalaRouter = express.Router();
-
+const pool = require('../modules/pool')
 // DB CONNECTION
 
 
 // GET
+router.get('/',(req,res)=>{
+    let queryText = 'SELECT * FROM "koalas";';
+
+    pool.query(queryText)
+    .then((result)=>{
+        res.send(result.rows);
+
+    }).catch((err)=>{
+        console.log('Error making query', queryText, err);
+        res.sendStatus(500);
+        
+    })
+
+
+
+
+
+})
 
 
 // POST
